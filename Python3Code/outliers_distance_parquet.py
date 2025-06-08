29ca1a269dc11488822c4b1197bc6712a578cec9
MichM072 <Michaelsmoor@outlook.com>
2025-06-08 20:38:38 +0200

    Added parquet support + parquet results

---
+++ Python3Code/outliers_distance_parquet.py
@@ -0,0 +1,121 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from Chapter3.OutlierDetection import DistanceBasedOutlierDetection
+from pathlib import Path
+
+"""
+TODO: Hugely simplify this process by accepting the concatenation of the dataframes as input.
+"""
+
+def sort_datasets(vehicles, files, folder='intermediate_datafiles'):
+    data_by_vehicle = {vehicle: [] for vehicle in vehicles}
+    for file in files:
+        for label in vehicles:
+            if label in file.lower():
+                path = os.path.join(folder, file)
+                df = pd.read_parquet(path).set_index('timestamp')
+                data_by_vehicle[label].append(df)
+                break
+    return data_by_vehicle
+
+def concat_data_per_vehicle(data_by_vehicle):
+    concatenated = {}
+    for vehicle, df_list in data_by_vehicle.items():
+        if df_list:
+            concatenated[vehicle] = pd.concat(df_list)
+        else:
+            concatenated[vehicle] = pd.DataFrame()
+    return concatenated
+
+def concat_data_filter(data_by_vehicle):
+    concatenated = {}
+    for vehicle, df_list in data_by_vehicle.items():
+        if df_list:
+            df = pd.concat(df_list)
+            
+            if not df.empty:
+
+                df.index = pd.to_datetime(df.index)
+                df = df.sort_index()
+
+                start_time = df.index[0]
+                end_time = df.index[-1]
+
+                start_time_plus_10s = start_time + pd.Timedelta(seconds=10)
+                end_time_minus_10s = end_time - pd.Timedelta(seconds=10)
+
+                df = df[(df.index > start_time_plus_10s) & (df.index < end_time_minus_10s)]
+            
+            concatenated[vehicle] = df
+        else:
+            concatenated[vehicle] = pd.DataFrame()
+    return concatenated
+
+
+##################################################
+# OUTLIERS
+##################################################
+
+
+sensor_prefixes = [
+    'acc_phone_',
+    'lin_acc_phone_',
+    'gyr_phone_',
+    'mag_phone_',
+]
+
+folder = 'intermediate_datafiles'
+output_folder = 'outliers2'
+vehicles = ['train', 'bus', 'metro', 'tram', 'car','walking']
+
+files = [f for f in os.listdir(folder) if f.endswith('.parquet')]
+data_by_vehicle = sort_datasets(vehicles, files, folder)
+raw_data = concat_data_per_vehicle(data_by_vehicle)
+concat_data = concat_data_filter(data_by_vehicle)
+
+outlier_detector = DistanceBasedOutlierDetection()
+
+cols_to_check_acc = ['acc_phone_X', 'acc_phone_Y', 'acc_phone_Z']
+cols_to_check_lin_acc = ['lin_acc_phone_X', 'lin_acc_phone_Y', 'lin_acc_phone_Z']
+cols_to_check_gyr = ['gyr_phone_X', 'gyr_phone_Y', 'gyr_phone_Z']
+cols_to_check_mag = ['mag_phone_X', 'mag_phone_Y', 'mag_phone_Z']
+cols_to_check_accuracy = ['location_phone_Horizontal Accuracy', 'location_phone_Vertical Accuracy']
+
+d_function = 'euclidean'
+vehicle_params = {
+    'train': {'dmin': 0.15, 'fmin': 0.1},
+    'bus': {'dmin': 0.5, 'fmin': 0.1},
+    'metro': {'dmin': 0.4, 'fmin': 0.1},
+    'tram': {'dmin': 0.45, 'fmin': 0.1},
+    'car': {'dmin': 0.1, 'fmin': 0.05},
+    'walking': {'dmin': 0.65, 'fmin': 0.1},
+}
+
+Path(output_folder).mkdir(parents=True, exist_ok=True)
+
+for vehicle, df in raw_data.items():
+    raw_output_file = os.path.join(output_folder, f"{vehicle}_raw.parquet")
+    df.to_parquet(raw_output_file, version='2.6', allow_truncated_timestamps=True)
+    print(f"Saved raw {vehicle} data to {raw_output_file}")
+
+
+for vehicle, df in concat_data.items():
+    print(f"Processing {vehicle} data...")
+
+    vehicle_dmin = vehicle_params[vehicle]['dmin']
+    vehicle_fmin = vehicle_params[vehicle]['fmin']
+
+    df = outlier_detector.simple_distance_based(df, cols=cols_to_check_acc, d_function=d_function, dmin=vehicle_dmin, fmin=vehicle_fmin)
+    df = outlier_detector.simple_distance_based(df, cols=cols_to_check_lin_acc, d_function=d_function, dmin=vehicle_dmin, fmin=vehicle_fmin)
+    df = outlier_detector.simple_distance_based(df, cols=cols_to_check_gyr, d_function=d_function, dmin=vehicle_dmin, fmin=vehicle_fmin)
+    df = outlier_detector.simple_distance_based(df, cols=cols_to_check_mag, d_function=d_function, dmin=vehicle_dmin, fmin=vehicle_fmin)
+
+    df_filtered = df[df['simple_dist_outlier'] == False] 
+
+##################################################
+##################################################
+
+    output_file = os.path.join(output_folder, f"{vehicle}_filtered.parquet")
+    df_filtered.to_parquet(output_file, version='2.6', allow_truncated_timestamps=True)
+    print(f"Saved filtered {vehicle} data to {output_file}")
