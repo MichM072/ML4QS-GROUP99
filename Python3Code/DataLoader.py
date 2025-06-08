c4d2e1efbaca4038af494f4d293e5e9a37db1ee6
MichM072 <Michaelsmoor@outlook.com>
2025-06-08 19:59:37 +0200

    Improved Dataloader to handle incorrect experiment setups and handle pauses parquet files to share results

--- Python3Code/DataLoader.py
+++ Python3Code/DataLoader.py
@@ -24,8 +24,8 @@ class PhyboxDatasetLoader:
 
     @staticmethod
     def set_dataset_path(student, exp_dir):
-        if os.path.exists(Path(f'./datasets/ML4QS-Vehicle/{exp_dir}/')):
-            return Path(f'./datasets/ML4QS-Vehicle/{exp_dir}/')
+        if os.path.exists(Path(f'./datasets/{exp_dir}/')):
+            return Path(f'./datasets/{exp_dir}/')
         else:
             return Path(f'/local/data/{student}/datasets/{exp_dir}/')
 
@@ -34,16 +34,61 @@ class PhyboxDatasetLoader:
         for col in df.columns:
             df.rename(columns={col: col.split('(')[0].strip()}, inplace=True)
 
+    @staticmethod
+    def create_labels_bandaid(start_time, end_time, experiment, exp_type):
+
+        df_labels = pd.DataFrame([{
+            'experiment': experiment.name,
+            'label': exp_type,
+            'label_start': int(start_time * 1_000_000_000),
+            'label_end': int(end_time),
+        }])
+
+        df_labels.to_csv(f'{experiment}/labels.csv', index=False)
+
+    @staticmethod
+    def create_labels(start_times, end_times, experiment, exp_types):
+
+        assert len(start_times) == len(end_times), "Missing end times for labels. Make sure that the number of start times and end times are equal."
+        assert len(start_times) >= len(exp_types), "There are more types than pauses, the experiment is incomplete."
+
+        rows = []
+
+        if len(start_times) == len(exp_types):
+            for start_time, end_time, exp_type in zip(start_times, end_times, exp_types):
+                rows.append({
+                    'experiment': experiment.name,
+                    'label': exp_type,
+                    'label_start': int(start_time * 1_000_000_000),
+                    'label_end': int(end_time * 1_000_000_000)
+                })
+
+        else:
+            for start_time, end_time in zip(start_times, end_times):
+                rows.append({
+                    'experiment': experiment.name,
+                    'label': exp_types,
+                    'label_start': int(start_time * 1_000_000_000),
+                    'label_end': int(end_time * 1_000_000_000)
+                })
+
+        df_labels = pd.DataFrame(rows)
+
+        df_labels.to_csv(f'{experiment}/labels.csv', index=False)
+
+
     def check_experiment_name(self, experiment_name):
         pattern = '|'.join(self.vehicles)
-        vehicle_name = re.search(pattern, experiment_name.lower())
+        vehicle_names = re.findall(pattern, experiment_name.lower())
 
-        return vehicle_name.group(0) if vehicle_name else None
+        return vehicle_names if vehicle_names else None
 
     def load_phybox_data(self):
 
         for experiment in self.dataset_path.iterdir():
-            start_time = None
+            start_times = []
+            end_times_exp = []
+            end_times = []
             if not experiment.is_dir():
                 continue
             exp_type = self.check_experiment_name(experiment.name)
@@ -51,27 +96,47 @@ class PhyboxDatasetLoader:
             with open(time_csv, 'r') as f:
                 for line in f.readlines():
                     if 'START' in line:
-                        start_time = float(line.split(',')[2])
+                        start_times.append(float(line.split(',')[2]))
+                    if 'PAUSE' in line:
+                        end_times.append(float(line.split(',')[2]))
+                        end_times_exp.append(float(line.split(',')[1]))
 
 
-            print(f'Experiment {experiment.name} has type {exp_type} and start time {start_time}')
+            # print(f'Experiment {experiment.name} has type {exp_type} and start time {start_time}')
 
+            if end_times:
+                self.create_labels(start_times, end_times, experiment, exp_type)
 
-            if start_time is not None:
+            if start_times:
                 print(f'Processing {experiment.name}')
+                latest_unix_timestamp = 0
                 for csv in (x for x in experiment.rglob('*.csv') if 'meta' not in str(x.parent)):
                     df = pd.read_csv(csv, delimiter=',')
-                    self.simplify_column_names(df)
 
-                    if df.columns.__contains__('unix_timestamp') and df.columns.__contains__('label'):
+                    if not df.columns.__contains__('Time'):
+                        # Bandaid fix to prevent invalid frames
                         continue
 
-                    # Ensure that the unix timestamp is in nanoseconds, whoops!
-                    df['unix_timestamp'] = ((df['Time'] + start_time) * 1_000_000_000).astype(np.int64)
+                    self.simplify_column_names(df)
 
-                    df['label'] = exp_type
+                    if df.columns.__contains__('unix_timestamp'):
+                        continue
+
+                    if end_times:
+                        df['unix_timestamp'] = 0
+                        for start_time, exp_time in zip(start_times, end_times_exp):
+                            mask = df['Time'] <= exp_time
+                            df.loc[mask, 'unix_timestamp'] = (
+                                        (df.loc[mask, 'Time'] + start_time) * 1_000_000_000).astype(np.int64)
+                    else:
+                        df['unix_timestamp'] = ((df['Time'] + start_times[0]) * 1_000_000_000).astype(np.int64)
+                        latest_unix_timestamp = max(latest_unix_timestamp, df['unix_timestamp'].max())
 
                     df.to_csv(csv, index=False)
+
+                if not end_times:
+                    self.create_labels_bandaid(start_times[0], latest_unix_timestamp, experiment, exp_type)
+
 
     def create_dataset(self, experiment_path, granularities=None, overwrite=True):
 
@@ -86,7 +151,7 @@ class PhyboxDatasetLoader:
 
         if not overwrite and os.path.exists(RESULT_PATH / RESULT_FNAME):
             print(f"Intermediate results file {RESULT_FNAME} already exists. Overwrite is set to False. Skipping this experiment and using available parquet.")
-            dataset = pd.read_parquet(RESULT_PATH / RESULT_FNAME, index_col=0)
+            dataset = pd.read_parquet(RESULT_PATH / RESULT_FNAME)
             return dataset
 
         # Set a granularity (the discrete step size of our time series data). We'll use a course-grained granularity of one
@@ -126,6 +191,8 @@ class PhyboxDatasetLoader:
             # We add the pressure sensed by the phone (continuous numerical measurements) and aggregate by averaging again
             dataset.add_numerical_dataset('Proximity.csv', 'unix_timestamp', ['Distance'], 'avg', 'proximity_phone_')
 
+            dataset.add_event_dataset('labels.csv', 'label_start', 'label_end', 'label', 'binary')
+
             # Get the resulting pandas data table
             dataset = dataset.data_table
 
@@ -152,8 +219,12 @@ class PhyboxDatasetLoader:
         # Make a table like the one shown in the book, comparing the two datasets produced.
         util.print_latex_table_statistics_two_datasets(datasets[0], datasets[1])
 
+        print(dataset.head())
+
         # Finally, store the last dataset we generated (250 ms).
-        dataset.to_parquet(RESULT_PATH / RESULT_FNAME, allow_truncated_timestamps=True)
+        dataset.reset_index(inplace=True)
+        dataset.rename(columns={'index': 'timestamp'}, inplace=True)
+        dataset.to_parquet(RESULT_PATH / RESULT_FNAME, version='2.6', allow_truncated_timestamps=True)
 
         # Lastly, print a statement to know the code went through
 
@@ -181,9 +252,9 @@ class PhyboxDatasetLoader:
 
 
 def main():
-    dataset_loader = PhyboxDatasetLoader('mmr497')
-    datasets = dataset_loader.create_all_datasets(overwrite=False)
-    datasets.to_parquet('./intermediate_datafiles/ML4QS_combined_results.parquet')
+    dataset_loader = PhyboxDatasetLoader('mmr497', exp_dir='ML4QS-Vehicle-2')
+    datasets = dataset_loader.create_all_datasets(overwrite=True)
+    datasets.to_parquet('./intermediate_datafiles/ML4QS_combined_results_2.parquet', version='2.6', allow_truncated_timestamps=True)
 
 if __name__ == '__main__':
     main()
