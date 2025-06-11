d82ab769ffc8163d9fd92aeb166f5f0bd9c59ab3
martinanfi <126090589+martinanfi@users.noreply.github.com>
2025-06-11 11:19:21 +0200

    fixed the for loop

--- Python3Code/outilier_viz.py
+++ Python3Code/outilier_viz.py
@@ -4,6 +4,12 @@ import pandas as pd
 raw_folder = 'raw_data'
 filtered_folder = 'outlier_data'
 
+#################################
+# percentages
+#################################
+
+
+
 results = []
 
 for filename in os.listdir(filtered_folder):
@@ -12,23 +18,23 @@ for filename in os.listdir(filtered_folder):
 for filename in os.listdir(raw_folder):
     raw_path = os.path.join(raw_folder, filename)
 
-    raw_df = pd.read_csv(raw_path, index_col=0, parse_dates=True)
-    filtered_df = pd.read_csv(filtered_path, index_col=0, parse_dates=True)
+raw_df = pd.read_csv(raw_path, index_col=0, parse_dates=True)
+filtered_df = pd.read_csv(filtered_path, index_col=0, parse_dates=True)
 
-    common_cols = raw_df.select_dtypes(include='number').columns.intersection(filtered_df.columns)
+common_cols = raw_df.select_dtypes(include='number').columns.intersection(filtered_df.columns)
 
-    for col in common_cols:
-        total = raw_df[col].notna().sum()
-        if total == 0:
-            continue 
-        n_nan = filtered_df[col].isna().sum()
-        pct_nan = (n_nan / total) * 100
+for col in common_cols:
+    total = raw_df[col].notna().sum()
+    if total == 0:
+        continue 
+    n_nan = filtered_df[col].isna().sum()
+    pct_nan = (n_nan / total) * 100
 
-        results.append({
-            'File': filename,
-            'Feature': col,
-            '% Outliers': round(pct_nan, 2)
-        })
+    results.append({
+        'File': filename,
+        'Feature': col,
+        '% Outliers': round(pct_nan, 2)
+    })
 
 outlier_summary = pd.DataFrame(results)
 outlier_summary.sort_values(by=['File', 'Feature'], inplace=True)
